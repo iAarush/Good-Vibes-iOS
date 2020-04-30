9529b48f1c84c9df61247a8db8ef4ebb0462175f
Aarush Mehrotra <aarushmehrotra7@gmail.com>
2020-05-01 00:18:02 +0530

    first commit

---
+++ Good Vibes App/Good Vibes App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Good Vibes App
+//
+//  Created by Aarush Mehrotra on 5/1/20.
+//  Copyright © 2020 Aarush Mehrotra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
